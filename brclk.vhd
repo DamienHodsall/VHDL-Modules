71435f9a95127e1d5500bd34b3a33df6f92e7b02
Damien Hodsall <damien.hodsall@gmail.com>
2023-12-05 17:23:09 -0800

    added brclk.vhd

---
+++ brclk.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+entity bclk is
+    port (
+        clkin : in std_logic;
+        reset : is std_logic;
+        period : in integer;
+        clkout : out std_logic
+    );
+end entity;
+
+architecture main of bclk is
+    signal count : integer;
+    signal clkint : std_logic;
+begin
+    process(clkin)
+    begin
+        count <= count + 1;
+        if reset = '1' then
+            count <= 0;
+        end if;
+
+        if rising_edge(clkin) then
+            if count >= period then
+                count <= 0;
+                clkint <= not clkint;
+            end if;
+        end if;
+    end process;
+    clkout <= clkint;
+end main;
